49137958c3155419f6215f73fdf303451dce40ca
Danny Cordoba <dannygcordobap@gmail.com>
2024-05-09 08:04:50 -0700

    Add region to watchtower logger

--- src/util/aws/__init__.py
+++ src/util/aws/__init__.py
@@ -5,11 +5,11 @@ from src.util.constants.aws import SESSION_PARAMETERS
 from src.util.constants.aws import SNS_EXCEPTION_SUBJECT
 from src.util.constants.aws import SNS_EXCEPTION_TOPIC_ARN
 
-_session = boto3.Session(**SESSION_PARAMETERS)
+boto3_session = boto3.Session(**SESSION_PARAMETERS)
 
 
 def post_exception_to_sns(exception_message):
-    sns = _session.client("sns", DEFAULT_REGION)
+    sns = boto3_session.client("sns", DEFAULT_REGION)
     sns.publish(TopicArn=SNS_EXCEPTION_TOPIC_ARN,
                 Message=exception_message,
                 Subject=SNS_EXCEPTION_SUBJECT)
--- src/util/logging/cloudwatch_logger.py
+++ src/util/logging/cloudwatch_logger.py
@@ -5,11 +5,15 @@ import watchtower
 
 import logging
 
+from src.util.aws import boto3_session
+from src.util.constants.aws import DEFAULT_REGION
+
 log_stream_name = f"application.{date.today().isoformat()}.log"
 log_group_name = "/ext/heroku/InternalAutomationService"
 
 logger = logging.getLogger()
 logger.addHandler(watchtower.CloudWatchLogHandler(
+    boto3_client=boto3_session.client("logs", region_name=DEFAULT_REGION),
     log_group_name=log_group_name,
     log_stream_name=log_stream_name
 ))
